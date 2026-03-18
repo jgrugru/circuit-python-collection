f50aaea146c37021192cdb6a50560cd9372e3b4f
jgrugru <jeff.gruenbaum@gmail.com>
2026-03-17 22:31:47 -0500

    working on oled

--- greentree/kmk/oled_ssd1306.py
+++ greentree/kmk/oled_ssd1306.py
@@ -1,41 +1,46 @@
-import board
 import busio
 import displayio
 from kmk.extensions import Extension
-from greentree.logger import Logger
 import time
+import microcontroller
 
 DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 64
-BUFFER_SIZE = DISPLAY_WIDTH * DISPLAY_HEIGHT // 8  # 1024 bytes
-buffer = bytearray(BUFFER_SIZE)  # all zeros
-frames = [
-    b"\x40" + open(f"/completed_animations/logo/frame{n}.bin", "rb").read()
-    for n in range(1, 5)
-]
+BUFFER_SIZE = DISPLAY_WIDTH * DISPLAY_HEIGHT // 8
 
 
 class OLED(Extension):
-    """
-    byte commands to ssd1306 over i2c
-    0xAE,       # display off
-    0xA1,       # horizontal flip
-    0xC8,       # vertical flip
-    0xAF        # display on
-    """
-
     def __init__(
         self,
+        scl: microcontroller.Pin,
+        sda: microcontroller.Pin,
         startup_animation: list[str],
-        logger: callable = None,
+        button_press_animation: list[str],
+        layer_1_filepath: str,
+        frequency: int = 400_000,
     ):
-        self.is_initialized = False
+        # initialize all attributes to None
+        self.scl = None
+        self.sda = None
+        self.frequency = None
+
+        self.startup_animation = None
+        self.layer_1_filepath = None
+
+        self.is_initialized = None
         self.i2c = None
         self.oled = None
-        if logger is not None:
-            self.logger = logger
-        else:
-            self.logger = Logger()
+
+        # assign actual values
+        self.scl = scl
+        self.sda = sda
+        self.frequency = frequency
+
+        self.startup_animation = startup_animation
+        self.button_press_animation = button_press_animation
+        self.layer_1_filepath = layer_1_filepath
+
+        self.is_initialized = False
 
     def on_runtime_enable(self, keyboard):
         pass
@@ -44,41 +49,76 @@ class OLED(Extension):
         pass
 
     def during_bootup(self, keyboard):
-        print(self.is_initialized)
         if not self.is_initialized:
-            print("*" * 100)
             displayio.release_displays()
-            i2c = busio.I2C(scl=board.GP29, sda=board.GP28, frequency=400_000)
-            i2c.unlock()
 
-            locked = False
-            while not locked:
-                locked = i2c.try_lock()
+            i2c = busio.I2C(scl=self.scl, sda=self.sda, frequency=self.frequency)
 
-            for frame in frames:
-                i2c.writeto(0x3C, frame)
+            while not i2c.try_lock():
+                pass
+
+            print([hex(x) for x in i2c.scan()])
+
+            addr = 0x3C
+
+            def cmd(c):
+                i2c.writeto(addr, bytes([0x00, c]))
+
+            cmd(0xAE)
+            cmd(0x20)
+            cmd(0x00)
+            cmd(0xB0)
+            cmd(0xC8)
+            cmd(0x00)
+            cmd(0x10)
+            cmd(0x40)
+            cmd(0x81)
+            cmd(0x7F)
+            cmd(0xA1)
+            cmd(0xA6)
+            cmd(0xA8)
+            cmd(0x3F)
+            cmd(0xA4)
+            cmd(0xD3)
+            cmd(0x00)
+            cmd(0xD5)
+            cmd(0x80)
+            cmd(0xD9)
+            cmd(0xF1)
+            cmd(0xDA)
+            cmd(0x12)
+            cmd(0xDB)
+            cmd(0x40)
+            cmd(0x8D)
+            cmd(0x14)
+            cmd(0xAF)
+
+            cmd(0xA0)
+            cmd(0xC0)
+
+            buffer = b"\xff" * 1024
+            i2c.writeto(addr, b"\x40" + buffer)
+
+            time.sleep(1)
+
+            for frame in self.startup_animation:
+                i2c.writeto(addr, frame)
                 time.sleep(0.7)
-            i2c.writeto(0x3C, b"\x40" + b"\xff" * 1024)  # fills the screen
-            i2c.unlock()
-            # oled = adafruit_ssd1306.SSD1306_I2C(DISPLAY_WIDTH, DISPLAY_HEIGHT, i2c, addr=0x3c)
-            # oled.fill(0)
-            # oled.rotate(False)
 
-            # self.oled = oled
+            i2c.writeto(addr, self.layer_1_filepath)
+
             self.i2c = i2c
             self.is_initialized = True
 
     def before_matrix_scan(self, keyboard):
-        """
-        Return value will be injected as an extra matrix update
-        """
         pass
 
     def after_matrix_scan(self, keyboard):
-        """
-        Return value will be replace matrix update if supplied
-        """
-        pass
+        if keyboard.matrix_update is not None:
+            for frame in self.button_press_animation:
+                self.i2c.writeto(0x3C, frame)
+            self.i2c.writeto(0x3C, self.layer_1_filepath)
+            # time.sleep(0.7)
 
     def before_hid_send(self, keyboard):
         pass
@@ -91,6 +131,3 @@ class OLED(Extension):
 
     def on_powersave_disable(self, keyboard):
         pass
-
-
-# endregion
